3c05c8dff86c21a9dcc374003368a44dbe0a93d9
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-19 11:50:41 -0800

    learn file base routing

---
+++ learning-nextjs/src/app/contact/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div>this is Contact Page</div>
+  )
+}
+
+export default ContactPage
---
+++ learning-nextjs/src/app/profile/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div>this is profile page</div>
+  )
+}
+
+export default ProfilePage
