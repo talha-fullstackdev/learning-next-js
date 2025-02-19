3b2d688dddb21a41b8dbac8fb8d45a41d93ca008
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-19 12:26:03 -0800

    learn about nested routes

---
+++ learning-nextjs/src/app/profile/profileone/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfileOnePage = () => {
+  return (
+    <div>profile page one</div>
+  )
+}
+
+export default ProfileOnePage
---
+++ learning-nextjs/src/app/profile/profiletwo/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfileTwoPage = () => {
+  return (
+    <div>profile page two</div>
+  )
+}
+
+export default ProfileTwoPage
