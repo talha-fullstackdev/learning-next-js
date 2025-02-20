c7c99662e47722ba0e69ab739a39ad9e9f435d51
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-20 13:06:20 -0800

    learn linking in next js using LINK

--- learning-nextjs/src/app/page.js
+++ learning-nextjs/src/app/page.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="">
     <h1>learning next js</h1>
+    <Link className="ml-2" href="about">about</Link>
+    <Link className="ml-2" href="blog">blog</Link>
+    <Link className="ml-2" href="contact">contact</Link>
+    <Link className="ml-2" href="profile">profile</Link>
+    <Link className="ml-2" href="products">products</Link>
+    <Link className="ml-2" href="user">user</Link>
     </div>
   );
 }
