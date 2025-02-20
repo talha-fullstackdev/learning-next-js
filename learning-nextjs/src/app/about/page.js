0f0c144520e5180181562ce0f3b832478f77b51c
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-20 13:32:46 -0800

    learn about replace attribute in Link componnet

--- learning-nextjs/src/app/about/page.js
+++ learning-nextjs/src/app/about/page.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 const AboutPage = () => {
   return (
-    <div>this is  About Page</div>
+    <div>
+      <h1>this is  About Page</h1>
+      <Link href="/" className='text-blue-400 hover:underline hover:underline-offset-4' title='back home?'>back to home</Link>
+    </div>
+    
   )
 }
 
