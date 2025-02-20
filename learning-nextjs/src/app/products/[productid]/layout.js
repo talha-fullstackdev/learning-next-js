d33265c5ea5466e4f6e0fb1ce996ca91664630cf
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-20 11:42:16 -0800

    made layout component with arrow function and default export

--- learning-nextjs/src/app/products/[productid]/layout.js
+++ learning-nextjs/src/app/products/[productid]/layout.js
@@ -1,9 +1,13 @@
-export default function ProductsLayout({ children }) {
-    return (
-      <div>
-        <h1>products header</h1>
-        <main>{children}</main>
-        <h1>products footer</h1>
-      </div>
-    );
-  }
+
+import React from 'react'
+const layout = ({children}) => {
+  return (
+    <div>
+        <h1 className='text-4xl mb-4 '>products details</h1>
+        {children}
+        <h1 className='text-4xl mt-4'>Feature products</h1>
+    </div>
+  )
+}
+
+export default layout
