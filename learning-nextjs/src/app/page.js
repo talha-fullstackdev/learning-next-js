5bc10e9400fc2c57b29a26cbc2325615b8b49be8
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-20 13:23:11 -0800

    added basic styling to link buttons

--- learning-nextjs/src/app/page.js
+++ learning-nextjs/src/app/page.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
 export default function Home() {
+  const linkStyles = "ml-2 text-blue-400 hover:underline hover:underline-offset-4"
   return (
     <div className="">
     <h1>learning next js</h1>
-    <Link className="ml-2" href="/about">about</Link>
-    <Link className="ml-2" href="/blog">blog</Link>
-    <Link className="ml-2" href="/contact">contact</Link>
-    <Link className="ml-2" href="/profile">profile</Link>
-    <Link className="ml-2" href="/products">products</Link>
-    <Link className="ml-2" href="/user">user</Link>
-    <Link className="ml-2" href="/address/city">address</Link>
+    <Link className={linkStyles} href="/about">about</Link>
+    <Link className={linkStyles} href="/blog">blog</Link>
+    <Link className={linkStyles} href="/contact">contact</Link>
+    <Link className={linkStyles} href="/profile">profile</Link>
+    <Link className={linkStyles} href="/products">products</Link>
+    <Link className={linkStyles} href="/user">user</Link>
+    <Link className={linkStyles} href="/address/city">address</Link>
     </div>
   );
 }
