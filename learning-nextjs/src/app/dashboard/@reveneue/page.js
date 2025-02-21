3b05f43664e1ca632ad84ca04ba42e6473d06f47
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-21 12:56:59 -0800

    learn parallel routes in dashboard

---
+++ learning-nextjs/src/app/dashboard/@reveneue/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Reveneue = () => {
+  return (
+    <div>Reveneue</div>
+  )
+}
+
+export default Reveneue
---
+++ learning-nextjs/src/app/dashboard/layout.js
@@ -0,0 +1,26 @@
+import React from 'react'
+// import UserAnalytics from './@useranalytics/page'
+// import Reveneue from './@reveneue/page'
+// import Notifications from './@notifications/page'
+const layout = ({children,notifications,reveneue,useranalytics}) => { // we can also get routes like this without import
+  const styles = 'w-[200px] h-[200px]  text-center pt-20 text-xl font-semibold rounded-lg'
+  return (
+    <div>
+        <h1 className='text-2xl font-bold'>header dashboard</h1>
+        {children}
+        <div className="flex gap-10 mt-10 ml-10 mb-10 ">
+        <div className={`${styles} bg-lime-200`}>{notifications}</div>
+        <div className={`${styles} bg-blue-200`}>{reveneue}</div>
+        <div className={`${styles} bg-red-200`}> {useranalytics}</div>
+        </div>
+        
+     
+        {/* <UserAnalytics/>
+        <Reveneue/>
+        <Notifications/> */}
+        <h1 className='text-2xl font-bold'>Footer dashboard</h1>
+    </div>
+  )
+}
+
+export default layout
