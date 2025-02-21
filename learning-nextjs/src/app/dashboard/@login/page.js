84bc47584f5c16fd58fd6c11de1b958fa86f221f
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-21 14:56:45 -0800

    learn conditional routes using login logout functionality in parallel routes section

--- learning-nextjs/src/app/dashboard/@login/page.js
+++ learning-nextjs/src/app/dashboard/@login/page.js
@@ -1,9 +1,11 @@
+"use client"
 import React from 'react'
-
-const Login = () => {
+const Login = ({handleLogin}) => {
+    
   return (
     <div>
         <p>please login to continue</p>
+        <button title='login?' onClick={handleLogin} className='bg-lime-300 mt-2 p-2 px-4 rounded-lg ml-4'>login</button>
     </div>
   )
 }
--- learning-nextjs/src/app/dashboard/layout.js
+++ learning-nextjs/src/app/dashboard/layout.js
@@ -1,12 +1,22 @@
+"use client"
 import React from 'react'
 // import UserAnalytics from './@useranalytics/page'
 // import Reveneue from './@reveneue/page'
 // import Notifications from './@notifications/page'
+import { useState } from 'react'
 import Login from './@login/page'
 const layout = ({children,notifications,reveneue,useranalytics}) => { // we can also get routes like this without import
+  const [login,setLogin]=useState(false)
   const styles = 'w-[200px] h-[200px]  text-center pt-20 text-xl font-semibold rounded-lg'
-  const isLogin = false
-  return isLogin ?(
+  const handleLogin =()=>{
+         setLogin(true)
+         alert("login success")
+  }
+  const handleLogout =()=>{
+         setLogin(false)
+         alert("logout success")
+  }
+  return login ?(
     <div>
         <h1 className='text-2xl font-bold'>header dashboard</h1>
         {children}
@@ -15,14 +25,14 @@ const layout = ({children,notifications,reveneue,useranalytics}) => { // we can 
         <div className={`${styles} bg-blue-200`}>{reveneue}</div>
         <div className={`${styles} bg-red-200`}> {useranalytics}</div>
         </div>
-        
+        <button title='logout?' className='bg-lime-300 mt-2 p-2 px-4 rounded-lg ml-4' onClick={handleLogout}>logout</button>
      
         {/* <UserAnalytics/>
         <Reveneue/>
         <Notifications/> */}
         <h1 className='text-2xl font-bold'>Footer dashboard</h1>
     </div>
-  ):<Login/>
+  ):<Login handleLogin={handleLogin}/>
 }
 
 export default layout
