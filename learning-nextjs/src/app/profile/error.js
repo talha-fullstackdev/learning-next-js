a27a1cbfa129ee24a22a2a197d9b9b3ce3c93595
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-21 12:05:19 -0800

    learn about reset function in error file

--- learning-nextjs/src/app/profile/error.js
+++ learning-nextjs/src/app/profile/error.js
@@ -1,10 +1,11 @@
 "use client"
 import React from 'react'
-const error = ({error}) => {
+const error = ({error,reset}) => {
   return (
     <div>
         <p>error has occured</p>
         <p>{error.message}</p>
+        <button onClick={reset}>try again</button> //reset the error (some error could be fixed by simply trying again reset is for that)
     </div>
   )
 }
