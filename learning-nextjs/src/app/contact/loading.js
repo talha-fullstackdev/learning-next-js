8b3a43bac78aa6aae364f418c1516ca694d43c4f
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-06 18:01:36 +0500

    updated

--- learning-nextjs/src/app/contact/loading.js
+++ learning-nextjs/src/app/contact/loading.js
@@ -1,8 +1,9 @@
 export default function Loading() {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500">loading</div>
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500">
+        loading
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- learning-nextjs/src/app/contact/page.js
+++ learning-nextjs/src/app/contact/page.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const ContactPage = () => {
-  return (
-    <div>this is Contact Page</div>
-  )
-}
+  return <div>this is Contact Page</div>;
+};
 
-export default ContactPage
+export default ContactPage;
