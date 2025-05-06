70ab3e31a5d97719019e00c24af6a8ec83887f4c
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-06 18:00:59 +0500

    updated

--- learning-nextjs/src/app/dashboard/@login/page.js
+++ learning-nextjs/src/app/dashboard/@login/page.js
@@ -1,13 +1,18 @@
-"use client"
-import React from 'react'
-const Login = ({handleLogin}) => {
-    
+"use client";
+import React from "react";
+const Login = ({ handleLogin }) => {
   return (
     <div>
-        <p>please login to continue</p>
-        <button title='login?' onClick={handleLogin} className='bg-lime-300 mt-2 p-2 px-4 rounded-lg ml-4'>login</button>
+      <p>please login to continue</p>
+      <button
+        title="login?"
+        onClick={handleLogin}
+        className="bg-lime-300 mt-2 p-2 px-4 rounded-lg ml-4"
+      >
+        login
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
