57e514633b9813a74382232e4ef54c42f5e3f2c2
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-06 18:02:39 +0500

    updated

--- learning-nextjs/src/app/about/page.js
+++ learning-nextjs/src/app/about/page.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 const AboutPage = () => {
   return (
     <div>
-      <h1>this is  About Page</h1>
-      <Link href="/" className='text-blue-400 hover:underline hover:underline-offset-4' title='back home?'>back to home</Link>
+      <h1>this is About Page</h1>
+      <Link
+        href="/"
+        className="text-blue-400 hover:underline hover:underline-offset-4"
+        title="back home?"
+      >
+        back to home
+      </Link>
     </div>
-    
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
