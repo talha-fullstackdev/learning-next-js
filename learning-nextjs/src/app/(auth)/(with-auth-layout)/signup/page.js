387526fff4d9b354d4c5184cafbe6822df510037
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-06 18:03:05 +0500

    updated

--- learning-nextjs/src/app/(auth)/(with-auth-layout)/signup/page.js
+++ learning-nextjs/src/app/(auth)/(with-auth-layout)/signup/page.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const SignUpPage = () => {
-  return (
-    <div> signup page</div>
-  )
-}
+  return <div> signup page</div>;
+};
 
-export default SignUpPage
+export default SignUpPage;
